572db5b970de101d2efb31c984040eb768d086f0
tombo314 <u_tombo@icloud.com>
2023-01-19 16:55:35 +0900

    [Add] server.jsのルーティング関係

--- server.js
+++ server.js
@@ -2,15 +2,46 @@ let http = require("http");
 let fs = require("fs");
 let socket = require("socket.io");
 let server = http.createServer((req, res)=>{
+    // top
     if (req.url=="/"){
         res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(fs.readFileSync("index.html"));
-    } else if (req.url=="/main.css"){
+        res.end(fs.readFileSync("top/top.html"));
+    } else if (req.url=="/top/top.css"){
         res.writeHead(200, {"Content-Type": "text/css"});
-        res.end(fs.readFileSync("main.css"));
-    } else if (req.url=="/main.js"){
+        res.end(fs.readFileSync("top/top.css"));
+    } else if (req.url=="/top/top.js"){
         res.writeHead(200, {"Content-Type": "application/javascript"});
-        res.end(fs.readFileSync("main.js"));
+        res.end(fs.readFileSync("top/top.js"));
+    }
+    // standby
+    else if (req.url=="/standby"){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(fs.readFileSync("standby/standby.html"));
+    } else if (req.url=="/standby/standby.css"){
+        res.writeHead(200, {"Content-Type": "text/css"});
+        res.end(fs.readFileSync("standby/standby.css"));
+    } else if (req.url=="/standby/standby.js"){
+        res.writeHead(200, {"Content-Type": "application/javascript"});
+        res.end(fs.readFileSync("standby/standby.js"));
+    }
+    // battle
+    else if (req.url=="/battle"){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(fs.readFileSync("battle/battle.html"));
+    } else if (req.url=="/battle/battle.css"){
+        res.writeHead(200, {"Content-Type": "text/css"});
+        res.end(fs.readFileSync("battle/battle.css"));
+    } else if (req.url=="/battle/battle.js"){
+        res.writeHead(200, {"Content-Type": "application/javascript"});
+        res.end(fs.readFileSync("battle/battle.js"));
+    }
+    // data
+    else if (req.url=="/battle/data/data.json"){
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(fs.readFileSync("battle/data/data.json"));
     }
 }).listen(process.env.PORT || 8000);
 let io = socket(server);
+
+io.on("connection", (socket)=>{
+});
