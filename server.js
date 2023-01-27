ae7b6ad26b63b49603c7d0a39a908e59d1e8eb78
tombo314 <u_tombo@icloud.com>
2023-01-27 18:05:26 +0900

    [Update] ==から===へ

--- server.js
+++ server.js
@@ -7,55 +7,55 @@ let socket = require("socket.io");
 let server = http.createServer((req, res)=>{
     // ルーティング //////
     // top
-    if (req.url=="/"){
+    if (req.url==="/"){
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(fs.readFileSync("top/top.html"));
-    } else if (req.url=="/top/stylesheet/top.css"){
+    } else if (req.url==="/top/stylesheet/top.css"){
         res.writeHead(200, {"Content-Type": "text/css"});
         res.end(fs.readFileSync("top/stylesheet/top.css"));
-    } else if (req.url=="/top/top.js"){
+    } else if (req.url==="/top/top.js"){
         res.writeHead(200, {"Content-Type": "application/javascript"});
         res.end(fs.readFileSync("top/top.js"));
     }
     // standby
-    else if (req.url=="/standby"){
+    else if (req.url==="/standby"){
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(fs.readFileSync("standby/standby.html"));
-    } else if (req.url=="/standby/stylesheet/standby.css"){
+    } else if (req.url==="/standby/stylesheet/standby.css"){
         res.writeHead(200, {"Content-Type": "text/css"});
         res.end(fs.readFileSync("standby/stylesheet/standby.css"));
-    } else if (req.url=="/standby/standby.js"){
+    } else if (req.url==="/standby/standby.js"){
         res.writeHead(200, {"Content-Type": "application/javascript"});
         res.end(fs.readFileSync("standby/standby.js"));
     }
     // battle
-    else if (req.url=="/battle"){
+    else if (req.url==="/battle"){
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(fs.readFileSync("battle/battle.html"));
-    } else if (req.url=="/battle/stylesheet/battle.css"){
+    } else if (req.url==="/battle/stylesheet/battle.css"){
         res.writeHead(200, {"Content-Type": "text/css"});
         res.end(fs.readFileSync("battle/stylesheet/battle.css"));
-    } else if (req.url=="/battle/battle.js"){
+    } else if (req.url==="/battle/battle.js"){
         res.writeHead(200, {"Content-Type": "application/javascript"});
         res.end(fs.readFileSync("battle/battle.js"));
     }
     // sqlite
-    else if (req.url=="/sqlite/sqlite.js"){
+    else if (req.url==="/sqlite/sqlite.js"){
         res.writeHead(200, {"Content-Type": "application/javascript"});
         res.end(fs.readFileSync("sqlite/sqlite.js"));
     }
     // data
-    else if (req.url=="/battle/data/data.json"){
+    else if (req.url==="/battle/data/data.json"){
         res.writeHead(200, {"Content-Type": "application/json"});
         res.end(fs.readFileSync("battle/data/data.json"));
     }
     // images
-    else if (req.url=="/images/"){
+    else if (req.url==="/images/"){
         res.writeHead(200, {"Content-Type": "image/"});
         // res.end();
     }
     // music
-    else if (req.url=="/music/"){
+    else if (req.url==="/music/"){
         res.writeHead(200, {"Content-Type": "music/"});
         // res.end();
     }
