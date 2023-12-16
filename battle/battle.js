dffb3b9c750595a2010ca5a6cc1613d4608a80b0
tombo314 <u_tombo@icloud.com>
2023-12-17 01:12:24 +0900

    Add count down

--- battle/battle.js
+++ battle/battle.js
@@ -1,7 +1,20 @@
 // エレメントを取得する
 let elemUsernameFriend = document.getElementById("js-username-friend");
+let elemTime = document.getElementById("js-time");
 
 // 変数を宣言・初期化する
 let username = sessionStorage.getItem("username");
 
 elemUsernameFriend.textContent = username;
+
+let startCountDown = (time_sec)=>{
+    let set = setInterval(() => {
+        let min = Math.floor(time_sec/60);
+        let sec = time_sec%60;
+        elemTime.textContent = `00${min}`.slice(-2)+":"+`00${sec}`.slice(-2);
+        if (time_sec==0){
+            clearInterval(set);
+        }
+        time_sec--;
+    }, 1000);
+};
